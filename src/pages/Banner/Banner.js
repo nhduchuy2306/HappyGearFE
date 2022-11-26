118fcf319e5a8c6aed15701745a11428a4c6175b
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2022-11-26 14:13:20 +0700

    add banner for policy

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import "./Banner.scss"
 
-function Banner(props) {
+function Banner({ image, title, name }) {
   return (
-    <section className="breadcrumb-section set-bg" style={{backgroundImage: `url(${props.image})`}}>
+    <section className="breadcrumb-section set-bg" style={{backgroundImage: `url(${image})`}}>
         <div className="container">
             <div className="row">
                 <div className="col-lg-12 text-center">
                     <div className="breadcrumb__text">
-                        <h2>{props.title===""?"HappyGear Shop":props.title}</h2>
+                        <h2>{title===""?"HappyGear Shop":title}</h2>
                         <div className="breadcrumb__option">
                             <Link to="/">Home</Link>
-                            <span>{props.name}</span>
+                            <span>{name}</span>
                         </div>
                     </div>
                 </div>
